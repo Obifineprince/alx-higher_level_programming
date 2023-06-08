c28e3db7466ad311de2245d7ca1f5ee1260889fd
Obifineprince <ifeanyiprincewill070@gmail.com>
2023-06-08 13:08:55 -0700

    importing add function

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+# Import the function from add_0.py
+from add_0 import add
+
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Perform the addition and store the result
+result = add(a, b)
+
+# Print the result using string formatting
+print("{} + {} = {}".format(a, b, result))
+
