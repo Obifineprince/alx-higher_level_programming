283d6d3ba260f08b983b3fbb18a70ce9d1eef35f
Obifineprince <ifeanyiprincewill070@gmail.com>
2023-06-09 00:23:37 -0700

    correction on cal.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,9 @@
-#!/usr/bin/pyython3
-from calculator_1 import add, sub, div, mul
-
-10 = a
-5 = b
-
-#print the re3sult 
-
-print ("{} + {} = {}".format(a, b, add(a, b)))
-print ("{} - {} = {}".format(a, b, sub(a, b)))
-print ("{} / {} = {}".format(a, b, div(a, b)))
-print ("{} * {} = {}".format(a, b, mul(a, b)))
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, div, mul, sub
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
