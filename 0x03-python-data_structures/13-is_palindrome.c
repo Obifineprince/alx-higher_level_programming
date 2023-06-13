40ded5ef94d3d74190d1248eec4ec6d657bac2ce
Obifineprince <ifeanyiprincewill070@gmail.com>
2023-06-13 07:39:56 -0700

    print a matrix of integers in a specific format

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,50 +1,60 @@
 #include "lists.h"
+#include <stdio.h>
+
+int is_palindrome(listint_t **head);
+
+{
+listint_t *nhead, *tort, *hare, *ptort;
+listint_t *cut = NULL, *half, *it1, *it2;
+if (!head || !*head)
+return (1);
+nhead = *head;
+if (nhead->next != NULL)
+{
+for (hare = nhead, tort = nhead; hare != NULL && hare->next != NULL;
+ptort = tort, tort = tort->next)
+hare = hare->next->next;
+if (hare != NULL)
+{
+cut = tort;
+tort = tort->next;
+}
+ptort->next = NULL;
+half = tort;
+it1 = reverse_listint(&half);
+for (it2 = *head; it2; it1 = it1->next, it2 = it2->next)
+{
+if (it2->n != it1->n)
+return (0);
+}
+if (cut == NULL)
+ptort->next = half;
+else
+{
+ptort->next = cut;
+cut->next = half;
+}
+}
+return (1);
+}
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Double pointer to the head of the linked list.
- *
- * Return: 1 if the linked list is a palindrome, 0 otherwise.
- * An empty list is considered a palindrome.
- */
-int is_palindrome(listint_t **head)
+* reverse_listint - Reverses a linked list in place
+* @head: Pointer to the pointer pointing to the first item in the list
+* Return: The new head of the reversed list
+*/
+listint_t *reverse_listint(listint_t **head)
 {
-	if (*head == NULL)
-	{
-		return 1;
-	}
-
-	int length = 0;
-	listint_t *current = *head;
-	while (current != NULL)
-	{
-		length++;
-		current = current->next;
-	}
-
-	int stack[length / 2];
-	int top = -1;
-
-	current = *head;
-	for (int i = 0; i < length / 2; i++)
-	{
-		stack[++top] = current->n;
-		current = current->next;
-	}
-
-	if (length % 2 != 0)
-	{
-		current = current->next;
-	}
-
-	while (current != NULL)
-	{
-		if (current->n != stack[top--])
-		{
-			return 0;
-		}
-		current = current->next;
-	}
-
-	return 1;
+listint_t *next = NULL, *prev = NULL;
+if (!head || !*head)
+return (NULL);
+while ((*head)->next)
+{
+next = (*head)->next;
+(*head)->next = prev;
+prev = *head;
+*head = next;
+}
+(*head)->next = prev;
+return (*head);
 }
