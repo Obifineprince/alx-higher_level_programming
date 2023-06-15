552d8dad0df55db59ccee42a744ffc4f97794d50
Obifineprince <ifeanyiprincewill070@gmail.com>
2023-06-14 17:12:44 -0700

    data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-	new_matrix = []
-
+    new_matrix = []
+    for row in matrix:
+        new_row = matrix
+        for num in row:
+            square_value = num ** 2
+            new_row.append(square_value)
+        new_matrix.append(new_row)
+    return (new_matrix)
