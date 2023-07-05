02a097122eb25f57e92d1fb2fec7c0a9d01dd75d
Obifineprince <ifeanyiprincewill070@gmail.com>
2023-07-05 02:46:45 -0700

    function defines a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+"""
+Defines a rectangle class
+"""
+
+
 class Rectangle:
     """
-    This is an empty class representing a rectangle.
+    defines a rectangle shape
     """
-
-    def __init__(self):
-        """
-        This is the constructor method for the Rectangle class.
-        It initializes a new instance of the class.
-        """
-        pass
+    pass
